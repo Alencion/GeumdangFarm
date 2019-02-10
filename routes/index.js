3a32e622e683422ed3b0c189cd222604b465ec58
gayeon1025 <kgy1559@gmail.com>
2019-02-10 17:56:28 +0900

    I corrected Jeongjun's typo! Be careful!!! he is baby :)

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index.pug');
+  res.render('Index/index.pug');
 });
-s
-module.exports = route;
+
+module.exports = router;
